b204e3d4d66adf402c8d8e0dab87daaa3e4f80de
Sebastian Eisenkrätzer <sebastian@Mac.fritz.box>
2026-05-25 22:00:27 +0200

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,16 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  const isAuthPage = request.nextUrl.pathname.startsWith('/login')
+  const token = request.cookies.get('sb-xxbgmcalobabafdrxjcn-auth-token')
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
   return NextResponse.next()
 }
 
