97fdde515afb59227debd820f70b1b11b23f36d0
Sebastian Eisenkrätzer <sebastian@Mac.fritz.box>
2026-05-26 21:11:24 +0200

    singleton supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,15 +1,13 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+let client: any = null
+
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-        detectSessionInUrl: true,
-      }
-    }
-  )
+  if (!client) {
+    client = createBrowserClient(
+      'https://xxbgmcalobabafdrxjcn.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4YmdtY2Fsb2JhYmFmZHJ4amNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2OTA0ODIsImV4cCI6MjA5NTI2NjQ4Mn0.XrD62q_DtiTmInz6SqnHlQ9QPQtZNDaVPATBteoZ9xg'
+    )
+  }
+  return client
 }
